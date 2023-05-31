3ebbdf4b19687c3d48a4eefced704a17fbbe5936
Saba <phruidzesaba@gmail.com>
2023-05-31 21:25:58 +0400

    and changed few things

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
+import { useReducer } from "react";
 import "./App.css";
+import Main from "./Components/Main";
+import WhatGender from "./Components/WhatGender";
+import HowMany from "./Components/HowMany";
+
+const initialState = { color: "blue" };
+
+type ActionType = {
+  type: string;
+  payload: string;
+};
+
+const reducer = (state: typeof initialState, action: ActionType) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case "sky":
+      newState.color = action.payload;
+      return newState;
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [start, dispatch] = useReducer(reducer, initialState);
+  const changeDispatch = (a: string, b: string) => {
+    dispatch({
+      type: a,
+      payload: b,
+    });
+  };
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <HowMany />
+      <WhatGender />
+      <Main />
+      <div
+        style={{ backgroundColor: start.color }}
+        onClick={() => {
+          changeDispatch("sky", "#70f1ff");
+        }}
+      >
+        grger
+      </div>
+    </div>
   );
 }
 
---
+++ src/Components/HowMany.tsx
@@ -0,0 +1,3 @@
+export default function HowMany() {
+  return <div>HowMany</div>;
+}
