71f9445592226aabcf9bf882941c9a42978882a0
Saba <phruidzesaba@gmail.com>
2023-06-04 07:15:16 +0400

    I have to make first two pages position:absolute in order to not cause any problem when aligning third page and other two pages

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,16 +1,32 @@
 import { useContext } from "react";
 import { myContext } from "../App";
-
+import Common from "./Common.module.css";
 export default function Main() {
   const myContext1 = useContext(myContext);
   const { start, changeDispatch } = myContext1;
 
   return (
-    <div>
+    <div className={Common.MainContainer}>
       <h1>See how many waters You drinked</h1>
       <h3>Goal: {start.which === "liter" ? start.litre : start.gender}iter</h3>
+      <div className={Common.bigCupContainer}>
+        <div className={Common.waterInsideContainer}>
+          <h3>{start.which === "liter" ? start.litre : start.gender}iter</h3>
+          <h4>Remained</h4>
+        </div>
+      </div>
+      <h4>Select the number of glasses you have dranked</h4>
       <div>
-        <h3>{start.which === "liter" ? start.litre : start.gender}iter</h3>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
