1b0e321ecfda1a41bbeabd7dbfbce6210a5a9d65
Saba <phruidzesaba@gmail.com>
2023-06-04 18:35:14 +0400

    Currently I can choose each small cup individually and if you clicked the filled 500ml cup then it will become empty. also the bigger the number is the less number cups will also be filled

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { myContext } from "../App";
 import Common from "./Common.module.css";
+
 export default function Main() {
   const myContext1 = useContext(myContext);
   const { start, changeDispatch } = myContext1;
   const dataSmallCups = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const handeClick = () => {
+  const [filledCups, setFilledCups] = useState<number[]>([]);
+
+  const handleClick = (data: number) => {
+    if (filledCups.includes(data)) {
+      // If the cup is already filled, remove it from filledCups
+      const updatedFilledCups = filledCups.filter((cup) => cup !== data);
+      setFilledCups(updatedFilledCups);
+    } else {
+      // Fill all the cups up to the clicked cup
+      const cupsToFill = dataSmallCups.slice(0, data);
+      setFilledCups(cupsToFill);
+    }
     changeDispatch("sCClicked", true);
   };
+
+  const isCupFilled = (data: number) => {
+    return filledCups.includes(data);
+  };
+
   return (
     <div className={Common.MainContainer}>
-      <h1>See how many waters You drinked</h1>
+      <h1>See how many waters You drank</h1>
       <h3>Goal: {start.which === "liter" ? start.litre : start.gender}iter</h3>
       <div className={Common.bigCupContainer}>
         <div className={Common.waterInsideBigCupContainer}>
@@ -21,24 +38,20 @@ export default function Main() {
         </div>
       </div>
       <h4 style={{ margin: "15px 0 5px 0" }}>
-        Select the number of glasses you have dranked
+        Select the number of glasses you have drunk
       </h4>
       <div className={Common.smallCupsContainer}>
         {dataSmallCups.map((data: number) => (
           <div
             className={Common.smallCupContainer}
             key={data}
-            onClick={handeClick}
+            onClick={() => handleClick(data)}
             style={{ paddingTop: !start.smallCupChanger ? "30px" : 0 }}
           >
-            {start.smallCupChanger === true ? (
-              <>
-                <div className={Common.waterInsideSmallCupContainer}>
-                  500 ML
-                </div>
-              </>
+            {isCupFilled(data) ? (
+              <div className={Common.waterInsideSmallCupContainer}>500 ML</div>
             ) : (
-              <>500 ML</>
+              <div className={Common.emptyInsideSmallCupContainer}>500 ML</div>
             )}
           </div>
         ))}
