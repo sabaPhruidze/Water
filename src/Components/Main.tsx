2bed32108d43889c6c746aa6db0be44bb31bd593
Saba <phruidzesaba@gmail.com>
2023-06-05 14:15:59 +0400

    I did new logic but Main problem did not solved

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,29 +1,22 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import { myContext } from "../App";
 import Common from "./Common.module.css";
 
 export default function Main() {
   const myContext1 = useContext(myContext);
   const { start, changeDispatch } = myContext1;
-  const dataSmallCups = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const [filledCups, setFilledCups] = useState<number[]>([]);
-
-  // const handleClick = (data: number) => {
-  //   if (filledCups.includes(data)) {
-  //     // If the cup is already filled, remove it from filledCups
-  //     const updatedFilledCups = filledCups.filter((cup) => cup !== data);
-  //     setFilledCups(updatedFilledCups);
-  //   } else {
-  //     // Fill all the cups up to the clicked cup
-  //     const cupsToFill = dataSmallCups.slice(0, data);
-  //     setFilledCups(cupsToFill);
-  //   }
-  //   changeDispatch("sCClicked", true);
-  // };
-
-  // const isCupFilled = (data: number) => {
-  //   return filledCups.includes(data); // I now have an individual small cup which I clicked
-  // };
+  const dataSmallCups =
+    start.litre === 2
+      ? [0, 1, 2, 3]
+      : start.gender == 2.7
+      ? [0, 1, 2, 3, 4]
+      : start.litre === 3
+      ? [0, 1, 2, 3, 4, 5]
+      : start.gender == 3.7
+      ? [0, 1, 2, 3, 4, 5, 6]
+      : start.litre === 4
+      ? [0, 1, 2, 3, 4, 5, 6, 7]
+      : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <>
@@ -36,48 +29,48 @@ export default function Main() {
             <h3>{!start.literOrGender ? start.litre : start.gender} L</h3>
             <h4> Remained</h4>
           </div>
-          <div className={Common.parcentage}></div>
+          <div className={Common.percent} style={{ height: 0 }}></div>
         </div>
         <h4 style={{ margin: "15px 0 5px 0" }}>
           Select the number of glasses you have drunk
         </h4>
         <div
           className={Common.glasses}
-          style={{ width: start.litre === 2 ? 200 : 10 }} // by this logic if it is 2 then it will show 2/2 glasses ...
+          style={{
+            width:
+              start.litre === 2
+                ? 200
+                : start.litre === 3
+                ? 280
+                : start.gender == 2.7
+                ? 280
+                : start.litre === 4
+                ? 350
+                : start.gender == 3.7
+                ? 350
+                : start.litre === 5
+                ? "100vw"
+                : "100vw",
+          }} // by this logic if it is 2 then it will show 2/2 glasses ...
         >
-          <div className={`${Common.glass} ${Common.smallGlass}`}>500 ML</div>
-          <div className={`${Common.glass} ${Common.smallGlass}`}>500 ML</div>
-          <div className={`${Common.glass} ${Common.smallGlass}`}>500 ML</div>
-          <div className={`${Common.glass} ${Common.smallGlass}`}>500 ML</div>
+          {dataSmallCups.map((data: number, idx: number) => (
+            <div
+              className={`${Common.glass} ${Common.smallGlass}`}
+              key={data}
+              style={{
+                backgroundColor: "white",
+                color: "#439de7",
+                display: "flex",
+                textAlign: "center",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              500 ML
+            </div>
+          ))}
         </div>
       </div>
     </>
   );
 }
-/*<div className={Common.bigCupContainer}>
-        <div className={Common.waterInsideBigCupContainer}>
-          <p style={{ fontSize: 20 }}>
-            {!start.literOrGender ? start.litre : start.gender}iter
-          </p>
-          <h4>Remained</h4>
-        </div>
-      </div>
-      <h4 style={{ margin: "15px 0 5px 0" }}>
-        Select the number of glasses you have drunk
-      </h4>
-      <div className={Common.smallCupsContainer}>
-        {dataSmallCups.map((data: number) => (
-          <div
-            className={Common.smallCupContainer}
-            key={data}
-            onClick={() => handleClick(data)}
-            style={{ paddingTop: !start.smallCupChanger ? "30px" : 0 }}
-          >
-            {isCupFilled(data) ? (
-              <div className={Common.waterInsideSmallCupContainer}>500 ML</div>
-            ) : (
-              <div className={Common.emptyInsideSmallCupContainer}>500 ML</div>
-            )}
-          </div>
-        ))}
-      </div> */
